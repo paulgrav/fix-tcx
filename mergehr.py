70ffe07a348f36d2ac96031779108e5439e92ad0
Paul Grave <paul@stomer.net>
2012-08-28 19:46:44 +0100

    now works

--- mergehr.py
+++ mergehr.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import sys
-import xml.etree.ElementTree
+import xml.etree.ElementTree as ET
 from xml.dom.minidom import parse, parseString
 import xml.parsers.expat
 import sys
@@ -16,6 +16,7 @@ class GPXParser:
 	_current_element = None
 	_current_datetime = None
 	_current_datastring = ""
+	_current_loc = {}
 
 	def __init__(self, filename):
 		self._data = {}
@@ -24,6 +25,7 @@ class GPXParser:
 		self._current_datetime = None
 		self._current_datastring = ""
 		self._filename = filename
+		self._current_loc = {}
 
 	def parse(self):
 		p = xml.parsers.expat.ParserCreate()
@@ -36,6 +38,8 @@ class GPXParser:
 
 	def _start_element(self, name, attrs):
 		self._current_element = name
+		if name == "trkpt":
+			self._current_loc = {"lon": attrs['lon'], "lat": attrs['lat']}
 
 	def _end_element(self, name):
 		data = self._current_datastring.strip()
@@ -46,11 +50,8 @@ class GPXParser:
 					self._current_datetime = datetime.strptime(data, "%Y-%m-%dT%H:%M:%SZ")
 				except ValueError:
 					self._current_datetime = data
-					print data
 
-		if name == "tp1:hr":
-			if data:
-				self._data[self._current_datetime] = data
+				self._data[self._current_datetime] = self._current_loc
 
 		self._current_datastring = ""
 
@@ -63,37 +64,33 @@ hr_info1 = parser.parse()
 parser2 = GPXParser(xml_file2)
 hr_info2 = parser2.parse()
 
+
 if hr_info1 and hr_info2:
 	print "Nothing todo"
 	sys.exit(0)
 
+
 if hr_info1:
 	tree = ET.parse(xml_file2)
+	ns = "http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2"
+	ET.register_namespace('', ns)
 	root = tree.getroot()
 
-	for child in root:
-		 print child.tag, child.attrib
-
-	for trkpt in doc.getElementsByTagName("trkpt"):
-		extensions = doc.createElement("extensions")
-		trackpointextention = doc.createElementNS("http://www.garmin.com/xmlschemas/TrackPointExtension/v1","tp1:TrackPointExtension")
-		hr = doc.createElementNS("http://www.garmin.com/xmlschemas/TrackPointExtension/v1","tp1:hr")
-		time = trkpt.getElementsByTagName('time')[0].firstChild.nodeValue
+	for child in root.iter("{%s}Trackpoint" % ns):
+		time = child.find('{%s}Time' % ns).text
 		time = datetime.strptime(time, "%Y-%m-%dT%H:%M:%SZ")
 		index = min(hr_info1, key=lambda date : abs(time-date))
+		loc = hr_info1[index]
 
-		hrValue = doc.createTextNode(hr_info1[index]);
-		hr.appendChild(hrValue)
-		trackpointextention.appendChild(hr)
-		extensions.appendChild(trackpointextention)
-		trkpt.appendChild(extensions)
+		lat_ele  = child.find("{{{0}}}Position/{{{0}}}LatitudeDegrees".format(ns))
+		lon_ele  = child.find("{{{0}}}Position/{{{0}}}LongitudeDegrees".format(ns))
 
-	print doc.toxml()
-#         <extensions>
-#           <tp1:TrackPointExtension>
-#             <tp1:hr>102</tp1:hr>
-#           </tp1:TrackPointExtension>
-#         </extensions>
+		if lat_ele is not None and lon_ele is not None:
+			lat_ele.text = loc['lat']
+			lon_ele.text = loc['lon']
+
+	tree.write(sys.stdout,  xml_declaration=True,encoding="utf-8", method="xml")
+
 
 if hr_info2:
 	pass
